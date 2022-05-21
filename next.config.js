f0f333f2a4d8ff11fb6c5fabe43c73dc0d815a5c
Cristian Reyes <cristianir.dev@gmail.com>
2022-05-21 03:08:02 -0300

    chore: add eslint config and chakra icons

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  eslint: {
+    dirs: ['components', 'layout', 'pages', 'hooks', 'lib'], // Only run ESLint on this directories during production builds (next build)
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
